1adf5dcb78af81817dacc4f2fa6e35e4a6c50748
sebastian guevara <Sebastian Guevara>
2023-02-28 14:43:13 -0500

    validando habitaciones

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,7 @@
 class Room < ApplicationRecord
+  validates :title, presence: true
+  validates :description, presence: true, length: { maximum: 400 }
+  validates :beds, presence: true, numericality: { only_integer: true }
+  validates :guests, presence: true, numericality: { only_integer: true }
+  validates :image_url, presence: true
 end
